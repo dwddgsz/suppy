acdb7d81038b9af1d3cb653556ea8207e719e0cf
dwddgsz <dwddgsz@gmail.com>
2020-11-09 21:48:29 +0100

    add:basic global styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+:root {
+  --blue:#a2d5f2;
+  --white:#fff;
+  --black:#1c1c1c;
+}
+* {
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+
+
+html,body {
+  height:100%;
+}
+
+html {
+  font-size:62.5%;
+  font-family: 'Montserrat', sans-serif;
+}
+
+body {
+  background-color: var(--blue);
+}
+
+a {
+  text-decoration:none;
+  color: var(--black);
+}
+
+ul {
+  list-style:none;
+}
+
+input:focus,button:focus {
+  outline:none;
+}
+
+
+`
 
 ReactDOM.render(
-    <App />,
+  <>
+  <GlobalStyle />
+    <App />
+    </>,
   document.getElementById('root')
 );
