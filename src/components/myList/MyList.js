c3280915528781abc4da2ca40be8110eee7eb8a9
dwddgsz <dwddgsz@gmail.com>
2020-11-11 19:20:42 +0100

    update:Component connected to firestore

--- src/components/myList/MyList.js
+++ src/components/myList/MyList.js
@@ -1,23 +1,52 @@
-import React from 'react'
+import React,{Component} from 'react'
 import CardStyled from '../styled/CardStyled';
 import ListStyled from '../styled/ListStyled';
 import TitleStyled from '../styled/TitleStyled';
+import firebase from '../../firebase/init';
+import {firebaseLooper} from '../../firebase/firestoreLooper';
 
 
-const MyList = () => {
-    return (
+class MyList extends Component{
+    state = {
+        data: [],
+    }
+
+     fetchData = () => {
+        const currentUserId = firebase.auth().currentUser.uid;
+
+        firebase
+        .firestore()
+        .collection('requests')
+        .where('userId','==',currentUserId)
+        .get()
+        .then(snapshot=>{
+            this.setState({data:firebaseLooper(snapshot)})
+    })  
+    }
+
+    componentDidMount(){
+        this.fetchData()
+    }
+
+    checkForRender = () => {
+        if (this.state.data.length !== 0) {
+            return this.state.data.map(element=>{
+                console.log(element)
+                return <CardStyled key={element.id} requestTitle={element.requestTitle} date={element.date} country={element.country} city={element.city} buttonMessege="done"/>
+            })
+        }
+    }
+
+    render(){
+        return (
         <>
         <TitleStyled>my list</TitleStyled>
         <ListStyled>
-            <CardStyled title='shopping' location='Poland, Warsaw' date='28.10.99 14:45' buttonMessage="done"/>
-            <CardStyled title='shopping' location='Poland, Warsaw' date='28.10.99 14:45' buttonMessage="done"/>
-            <CardStyled title='shopping' location='Poland, Warsaw' date='28.10.99 14:45' buttonMessage="done"/>
-            <CardStyled title='shopping' location='Poland, Warsaw' date='28.10.99 14:45' buttonMessage="done"/>
-            <CardStyled title='shopping' location='Poland, Warsaw' date='28.10.99 14:45' buttonMessage="done"/>
-            <CardStyled title='shopping' location='Poland, Warsaw' date='28.10.99 14:45' buttonMessage="done"/>
+            {this.checkForRender()}
         </ListStyled>
         </>
     )
+        }
 }
 
 export default MyList
